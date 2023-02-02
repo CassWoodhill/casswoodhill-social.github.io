e7666cffd180ad946b63ac197fa4230efef4a23d
Cass Woodhill <cwoodhill@kooth.com>
2023-02-02 17:47:56 +0000

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { App } from './App'
+
+const rootElement = document.getElementById('root')
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement,
+)
+
+// https://github.com/reactivando/template-react-typescript
